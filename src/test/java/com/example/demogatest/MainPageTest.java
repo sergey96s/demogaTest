a5009abae60abd0ee08ca6e96d93ce7c3f82abcb
Сергей Серебряков <serzh.serebryakov.96@gmail.com>
2023-06-25 23:23:18 +0700

    Initial commit

---
+++ src/test/java/com/example/demogatest/MainPageTest.java
@@ -0,0 +1,43 @@
+package com.example.demogatest;
+
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class MainPageTest {
+    private WebDriver driver;
+
+
+    @BeforeEach
+    public void setUp() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+    }
+
+    @AfterEach
+    public void tearDown() {
+        driver.quit();
+    }
+
+    @Test
+    public void enableButton() {
+        driver.get("https://demoqa.com/dynamic-properties");
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        WebElement displayedButton = driver.findElement(By.cssSelector("#visibleAfter"));
+        wait.until(ExpectedConditions.visibilityOf(displayedButton));
+        assertTrue(displayedButton.isDisplayed(), "Кнопка не стала видимой");
+    }
+}
